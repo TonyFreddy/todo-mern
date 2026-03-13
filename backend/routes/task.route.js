6f01822d9b7f651a6391475423718debd28f4907
Tony <kounleadissa@gmail.com>
2026-03-13 18:15:41 +0100

    structure correct for backend , add controller , route ...

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router =express.Router();
-const { getAllTask } = require('../controllers/task.controller');
+const { getAllTask , getSingleTask , createTask , updateTask , deleteTask } = require('../controllers/task.controller');
 
 
 
 //Get all the tasks
 router.get("/" , getAllTask);
+
+// get a single task
+router.get("/:id",getSingleTask);
+
+// Create a new task
+router.post("/" , createTask);
+
+// update a task 
+router.put("/:id", updateTask);
+
+//delete a task
+router.delete("/:id" , deleteTask);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -11,66 +11,6 @@ app.use(express.json());
 // @taskRoute
 app.use('/api/tasks' , taskRoute);
 
-
-// get one single task
-app.get('/api/tasks/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const task = await Task.findById(id);
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-// Create a new task
-app.post('/api/tasks', async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-
-// update a task 
-app.put('/api/tasks/:id', async (req, res) => {
-    try {
-         const { id } = req.params;
-        const task = await Task.findByIdAndUpdate(id ,req.body);
-
-        if (!task){
-            return res.status(404).json({ message: "Task not found" });
-        }
-
-        const updatedTask = await Task.findById(id);
-            res.status(200).json(updatedTask);
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-//delete a task
-app.delete ('/api/tasks/:id', async (req, res) => {
-    try {
-         const { id } = req.params;
-        const task = await Task.findByIdAndDelete(id);
-
-        if (!task){
-            return res.status(404).json({ message: "Task not found" });
-        }
-        
-            res.status(200).json({ message : "Task deleted successfuly"});
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-}); 
-
-
 mongoose.connect('mongodb://tony:tonydrzzdj@ac-psbcp8z-shard-00-00.xqet5tc.mongodb.net:27017,ac-psbcp8z-shard-00-01.xqet5tc.mongodb.net:27017,ac-psbcp8z-shard-00-02.xqet5tc.mongodb.net:27017/?ssl=true&replicaSet=atlas-5zuooj-shard-0&authSource=admin&appName=todo')
 .then(() => {
     console.log('Connected to the database');
