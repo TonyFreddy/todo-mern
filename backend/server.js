3fee80ff1aa40dc1d4e4849d6c3f84ec6fae0f35
Tony <kounleadissa@gmail.com>
2026-03-13 17:30:57 +0100

    add api to update and delete a task

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,43 @@ app.post('/api/tasks', async (req, res) => {
     }
 });
 
+
+// update a task 
+app.put('/api/tasks/:id', async (req, res) => {
+    try {
+         const { id } = req.params;
+        const task = await Task.findByIdAndUpdate(id ,req.body);
+
+        if (!task){
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        const updatedTask = await Task.findById(id);
+            res.status(200).json(updatedTask);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//delete a task
+app.delete ('/api/tasks/:id', async (req, res) => {
+    try {
+         const { id } = req.params;
+        const task = await Task.findByIdAndDelete(id);
+
+        if (!task){
+            return res.status(404).json({ message: "Task not found" });
+        }
+        
+            res.status(200).json({ message : "Task deleted successfuly"});
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}); 
+
+
 mongoose.connect('mongodb://tony:tonydrzzdj@ac-psbcp8z-shard-00-00.xqet5tc.mongodb.net:27017,ac-psbcp8z-shard-00-01.xqet5tc.mongodb.net:27017,ac-psbcp8z-shard-00-02.xqet5tc.mongodb.net:27017/?ssl=true&replicaSet=atlas-5zuooj-shard-0&authSource=admin&appName=todo')
 .then(() => {
     console.log('Connected to the database');
